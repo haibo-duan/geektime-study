8df6a03650215ce1f38f787a1785c4d3514c4e34
haibo.duan <dhaibo1986@sohu.com>
2021-08-05 20:09:32 +0800

    第二题的最终版本，优化掉了冗余代码，另外通过 反射调用hello方法

--- src/main/java/com/dhb/geektimestudy/kimmking/week1/HelloFileClassLoader.java
+++ src/main/java/com/dhb/geektimestudy/kimmking/week1/HelloFileClassLoader.java
@@ -1,42 +1,61 @@
 package com.dhb.geektimestudy.kimmking.week1;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Base64;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-public class HelloFileClassLoader extends ClassLoader{
+public class HelloFileClassLoader extends ClassLoader {
+
+	public static final String XLASS_NAME = "Hello.xlass";
+
+
 	public static void main(String[] args) {
 
 		try {
-			new HelloFileClassLoader().findClass("Hello").newInstance();
-		} catch (InstantiationException e) {
+			Class clazz = new HelloFileClassLoader().findClass("Hello");
+			Method hello = clazz.getDeclaredMethod("hello");
+			hello.invoke(clazz.newInstance());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
 			e.printStackTrace();
 		}
 
+
 	}
-	
+
+	/**
+	 * 解码 采用位运算
+	 * @param source
+	 * @return
+	 */
 	private byte[] decode(byte[] source) {
-		for(int i=0;i<source.length;i++) {
-			source[i] = (byte)(0xFFFF ^ source[i]);
+		for (int i = 0; i < source.length; i++) {
+			source[i] = (byte) (0xFFFF ^ source[i]);
 		}
 		return source;
 	}
-	
-	
-	
-	private byte[] getContent(String filePath) throws IOException{
-		File file = new File(filePath);
-		long fileSize = file.length();
-		FileInputStream fis = new FileInputStream(file);
+
+
+	/**
+	 * 根据传入的文件路径得到文件的二进制内容
+	 * @param filePath
+	 * @return
+	 * @throws IOException
+	 */
+	private byte[] getContent(String filePath) throws IOException {
+		FileInputStream fis = new FileInputStream(filePath);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		int i;
-		while ((i = fis.read()) !=- 1){
+		while ((i = fis.read()) != -1) {
 			baos.write(i);
 		}
 		fis.close();
@@ -45,15 +64,12 @@ public class HelloFileClassLoader extends ClassLoader{
 
 	@Override
 	protected Class<?> findClass(String name) throws ClassNotFoundException {
-
-		byte[] bytes = new byte[0];
 		try {
-			byte[] content = getContent(this.getClass().getClassLoader().getResource("Hello.xlass").getPath());
-			bytes = decode(content);
-			System.out.println(Base64.getEncoder().encodeToString(bytes));
-		} catch (IOException e) {
-			e.printStackTrace();
+			byte[] content = getContent(this.getClass().getClassLoader().getResource(XLASS_NAME).getPath());
+			byte[] bytes = decode(content);
+			return defineClass(name, bytes, 0, bytes.length);
+		} catch (Exception e) {
+			throw new ClassNotFoundException(name, e);
 		}
-		return defineClass(name,bytes,0,bytes.length);
 	}
 }
