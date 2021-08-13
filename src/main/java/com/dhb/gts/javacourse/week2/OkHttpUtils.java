9898b0a7c7ab794288e89f0d5eec7e6fc1bebd3d
haibo.duan <dhaibo1986@sohu.com>
2021-08-13 14:55:34 +0800

    使用OKHttp实现get post方法

---
+++ src/main/java/com/dhb/gts/javacourse/week2/OkHttpUtils.java
@@ -0,0 +1,55 @@
+package com.dhb.gts.javacourse.week2;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpUtils {
+
+	public static void main(String[] args) {
+		System.out.println(OkHttpUtils.httpGet("http://127.0.0.1:8808/test"));
+	}
+	
+	public static String httpGet(String url) {
+		OkHttpClient client = new OkHttpClient();
+		String result = null;
+		try {
+			Request request = new Request.Builder().url(url).build();
+			Response response = client.newCall(request).execute();
+			if(response.isSuccessful()){
+				result = response.body().string();
+			} else {
+				System.out.println("请求失败。。。");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public static String httpPost(String url,String strParam) {
+		OkHttpClient client = new OkHttpClient();
+		String result = null;
+		try {
+			MediaType type = MediaType.parse("application/json;charset=utf-8");
+			RequestBody body = RequestBody.create(type,strParam);
+			Request request = new Request.Builder()
+					.url(url)
+					.post(body)
+					.build();
+			Response response = client.newCall(request).execute();
+			if(response.isSuccessful()) {
+				result = response.body().string();
+			}else {
+				System.out.println("请求失败。。。");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
