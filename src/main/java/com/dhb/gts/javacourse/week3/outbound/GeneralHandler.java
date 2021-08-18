f8b5044ec6a7c299914f9de14243577c1509fd3f
haibo-duan <dhaibo1986@live.cn>
2021-08-18 13:53:52 +0800

    去除多余的import,并格式化代码

--- src/main/java/com/dhb/gts/javacourse/week3/outbound/GeneralHandler.java
+++ src/main/java/com/dhb/gts/javacourse/week3/outbound/GeneralHandler.java
@@ -23,12 +23,11 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class GeneralHandler extends ChannelInboundHandlerAdapter {
 
-	Map<String, String> head = new HashMap<String, String>();
-	Integer resLength = 0;
-	String respContent = "";
-
 	private final FullHttpRequest fullRequest;
 	private final ChannelHandlerContext inboundCtx;
+	private final Map<String, String> head = new HashMap<>();
+	private Integer resLength = 0;
+	private String respContent = "";
 
 	public GeneralHandler(FullHttpRequest fullRequest, ChannelHandlerContext inboundCtx) {
 		this.fullRequest = fullRequest;
@@ -37,7 +36,7 @@ public class GeneralHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if(msg instanceof HttpResponse) {
+		if (msg instanceof HttpResponse) {
 			HttpResponse response = (HttpResponse) msg;
 			for (Map.Entry entry : response.headers().entries()) {
 				head.put((String) entry.getKey(), (String) entry.getValue());
@@ -46,26 +45,26 @@ public class GeneralHandler extends ChannelInboundHandlerAdapter {
 				resLength = Integer.parseInt(response.headers().get("Content-Length"));
 			}
 		}
-		
-		if(msg instanceof HttpContent) {
+
+		if (msg instanceof HttpContent) {
 			HttpContent content = (HttpContent) msg;
 			ByteBuf buf = content.content();
 			respContent += buf.toString(CharsetUtil.UTF_8);
-			System.out.println("*************"+buf.toString(io.netty.util.CharsetUtil.UTF_8));
+			System.out.println("*************" + buf.toString(io.netty.util.CharsetUtil.UTF_8));
 			((HttpContent) msg).release();
 			if (respContent.getBytes().length >= resLength || !buf.isReadable()) {
 				ctx.channel().close();
-				handleResponse(fullRequest,inboundCtx);
+				handleResponse(fullRequest, inboundCtx);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 		super.exceptionCaught(ctx, cause);
 		Channel channel = ctx.channel();
-		if(channel.isActive()) {
+		if (channel.isActive()) {
 			System.out.println("出现异常，关闭连接。");
 			channel.close();
 		}
@@ -79,16 +78,16 @@ public class GeneralHandler extends ChannelInboundHandlerAdapter {
 	public void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx) {
 		FullHttpResponse response = null;
 		try {
-			byte[] body  = respContent.getBytes();
-			System.out.println("GeneralHandler response:"+respContent);
+			byte[] body = respContent.getBytes();
+			System.out.println("GeneralHandler response:" + respContent);
 			response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
 
-			if(head.isEmpty()) {
+			if (head.isEmpty()) {
 				response.headers().set("Content-Type", "application/json");
 				response.headers().setInt("Content-Length", resLength);
-			}else {
-				for(Map.Entry<String,String> entry:head.entrySet()){
-					response.headers().set(entry.getKey(),entry.getValue());
+			} else {
+				for (Map.Entry<String, String> entry : head.entrySet()) {
+					response.headers().set(entry.getKey(), entry.getValue());
 				}
 			}
 			response.headers().setInt("Content-Length", resLength);
--- src/main/java/com/dhb/gts/javacourse/week3/outbound/NettyAsyncHandler.java
+++ src/main/java/com/dhb/gts/javacourse/week3/outbound/NettyAsyncHandler.java
@@ -18,15 +18,14 @@ import io.netty.handler.codec.http.HttpResponseDecoder;
 import io.netty.handler.codec.http.HttpVersion;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 
-public class NettyAsyncHandler extends HttpOutBoundHandler{
+public class NettyAsyncHandler extends HttpOutBoundHandler {
 
 	private final EventLoopGroup workGroup;
 	private final Bootstrap bootstrap;
-	
-	
+
+
 	public NettyAsyncHandler(List<String> backendUrls) {
 		super(backendUrls);
 		workGroup = new NioEventLoopGroup(1);
@@ -34,9 +33,9 @@ public class NettyAsyncHandler extends HttpOutBoundHandler{
 		bootstrap.group(workGroup);
 		bootstrap.channel(NioSocketChannel.class);
 		bootstrap.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000);
-		bootstrap.option(EpollChannelOption.SO_REUSEPORT,true);
+		bootstrap.option(EpollChannelOption.SO_REUSEPORT, true);
 		bootstrap.option(ChannelOption.SO_RCVBUF, 1024 * 1024);
-		
+
 	}
 
 	@Override
@@ -45,16 +44,16 @@ public class NettyAsyncHandler extends HttpOutBoundHandler{
 			URI uri = new URI(url);
 			String domain = uri.getHost();
 			Integer port = uri.getPort();
-			DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,uri.toASCIIString());
+			DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.toASCIIString());
 			//如果请求的header为空 则构造header 反之将inbound请求的header传递到endpoint
-			if(inbound.headers().isEmpty()) {
+			if (inbound.headers().isEmpty()) {
 				request.headers().add("Host", domain);
 				request.headers().add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0");
 				request.headers().add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 				request.headers().add("Accept-Language", "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3");
 				request.headers().add("Connection", "keep-alive");
 				request.headers().add("Cache-Control", "max-age=0");
-			}else {
+			} else {
 				request.headers().set(inbound.headers());
 			}
 
@@ -65,12 +64,11 @@ public class NettyAsyncHandler extends HttpOutBoundHandler{
 					ch.pipeline().addLast(new HttpResponseDecoder());
 					//发送的时候 通过http编码
 					ch.pipeline().addLast(new HttpRequestEncoder());
-					ch.pipeline().addLast(new GeneralHandler(inbound,ctx));
+					ch.pipeline().addLast(new GeneralHandler(inbound, ctx));
 				}
 			};
-			ChannelFuture f;
 			bootstrap.handler(initializer);
-			f = bootstrap.connect(domain,port).sync();
+			ChannelFuture f = bootstrap.connect(domain, port).sync();
 			f.channel().writeAndFlush(request);
 			f.channel().closeFuture().sync();
 		} catch (Exception e) {
