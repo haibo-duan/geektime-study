5cb52406e81932736e9858b5f99c9db75bb65430
haibo-duan <dhaibo1986@live.cn>
2021-08-23 17:40:04 +0800

    采用Semaphore 实现第二题

---
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun11.java
@@ -0,0 +1,53 @@
+package com.dhb.gts.javacourse.week4;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class AsyncRun11 {
+	
+	private static final Semaphore semaphore = new Semaphore(0);
+
+	private static int sum() {
+		return fibo(36);
+	}
+
+	private static int fibo(int a) {
+		if(a<2){
+			return 1;
+		}
+		return fibo(a-1) + fibo(a-2);
+	}
+
+	static class SumThread extends Thread{
+		
+
+		private Integer result;
+
+		public Integer getResult() {
+			return result;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				result = sum();
+				semaphore.release();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		long start = System.currentTimeMillis();
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		semaphore.acquire();
+		
+		int result = sumThread.getResult();
+		System.out.println("异步计算结果："+result);
+		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
+	}
+
+}
