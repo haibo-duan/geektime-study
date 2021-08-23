2687e82d7ad4d9ec1c5ce496af6df5c7e238917b
haibo-duan <dhaibo1986@live.cn>
2021-08-23 18:03:41 +0800

    采用Exchanger来实现习题2，这样可以直接交换两个线程的数据，避免在线程中要定义成员变量

---
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun13.java
@@ -0,0 +1,44 @@
+package com.dhb.gts.javacourse.week4;
+
+import java.util.concurrent.Exchanger;
+
+public class AsyncRun13 {
+	
+	private static final Exchanger<Integer> exchanger = new Exchanger<>();
+
+	private static int sum() {
+		return fibo(36);
+	}
+
+	private static int fibo(int a) {
+		if(a<2){
+			return 1;
+		}
+		return fibo(a-1) + fibo(a-2);
+	}
+
+	static class SumThread extends Thread{
+		
+		
+		@Override
+		public void run() {
+			try {
+				int result = sum();
+				exchanger.exchange(result);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		long start = System.currentTimeMillis();
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		Integer result= exchanger.exchange(0);
+		
+		System.out.println("异步计算结果："+result);
+		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
+	}
+
+}
