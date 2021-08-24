30d95460954567e7f5ac51f8d9da95a0569edc03
haibo-duan <dhaibo1986@live.cn>
2021-08-24 19:51:52 +0800

    采用FutureTask实现

---
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun17.java
@@ -0,0 +1,50 @@
+package com.dhb.gts.javacourse.week4;
+
+import okhttp3.Call;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 采用 FutureTask实现
+ */
+public class AsyncRun17 {
+	
+
+	private static int sum() {
+		return fibo(36);
+	}
+
+	private static int fibo(int a) {
+		if(a<2){
+			return 1;
+		}
+		return fibo(a-1) + fibo(a-2);
+	}
+
+	static class SumThread implements Callable<Integer> {
+
+		@Override
+		public Integer call() throws Exception {
+			return sum();
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception{
+		long start = System.currentTimeMillis();
+		ExecutorService service = Executors.newCachedThreadPool();
+		
+		SumThread sumThread = new SumThread();
+		FutureTask<Integer> futureTask = new FutureTask<>(sumThread);
+		service.submit(futureTask);
+		service.shutdown();
+		System.out.println("异步计算结果："+futureTask.get());
+		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
+	}
+
+}
