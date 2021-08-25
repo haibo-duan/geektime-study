dfab1ebc25354ddc98c28e62154138332af3e83d
haibo-duan <dhaibo1986@live.cn>
2021-08-25 11:56:14 +0800

    采用ReentrantLock配合Condition来实现，类似于synchronized的wait/notify

--- src/main/java/com/dhb/gts/javacourse/week4/AsyncRun09.java
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun09.java
@@ -3,44 +3,26 @@ package com.dhb.gts.javacourse.week4;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * 采用ReentrantLock配合Condition来实现，类似于synchronized的wait/notify
+ */
 public class AsyncRun09 {
-	
+
 	private static final ReentrantLock lock = new ReentrantLock(true);
 	private static final Condition c1 = lock.newCondition();
-	
+
 	private static int sum() {
 		return fibo(36);
 	}
 
 	private static int fibo(int a) {
-		if(a<2){
+		if (a < 2) {
 			return 1;
 		}
-		return fibo(a-1) + fibo(a-2);
+		return fibo(a - 1) + fibo(a - 2);
 	}
 
-	static class SumThread extends Thread{
-		
-
-		private Integer result;
-
-		public Integer getResult() {
-			return result;
-		}
-		
-		@Override
-		public void run() {
-			lock.lock();
-			try {
-				result = sum();
-				c1.signalAll();
-			}finally {
-				lock.unlock();
-			}
-		}
-	}
-
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		long start = System.currentTimeMillis();
 		SumThread sumThread = new SumThread();
 		sumThread.start();
@@ -48,13 +30,33 @@ public class AsyncRun09 {
 		lock.lock();
 		try {
 			c1.await();
-		}finally {
+		} finally {
 			lock.unlock();
 		}
-		
+
 		int result = sumThread.getResult();
-		System.out.println("异步计算结果："+result);
-		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
+		System.out.println("异步计算结果：" + result);
+		System.out.println("计算耗时：" + (System.currentTimeMillis() - start) + "  ms");
 	}
 
+	static class SumThread extends Thread {
+
+
+		private Integer result;
+
+		public Integer getResult() {
+			return result;
+		}
+
+		@Override
+		public void run() {
+			lock.lock();
+			try {
+				result = sum();
+				c1.signalAll();
+			} finally {
+				lock.unlock();
+			}
+		}
+	}
 }
