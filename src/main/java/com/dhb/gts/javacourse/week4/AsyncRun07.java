0a7f32343709c900fb1427265bd82e2d4ab771cf
haibo-duan <dhaibo1986@live.cn>
2021-08-25 11:44:12 +0800

    采用 LockSupport的park/unpark来实现 格式化代码

--- src/main/java/com/dhb/gts/javacourse/week4/AsyncRun07.java
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun07.java
@@ -1,24 +1,37 @@
 package com.dhb.gts.javacourse.week4;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.LockSupport;
 
+/**
+ * 采用 LockSupport的park/unpark来实现
+ */
 public class AsyncRun07 {
-	
 	
 	private static int sum() {
 		return fibo(36);
 	}
 
 	private static int fibo(int a) {
-		if(a<2){
+		if (a < 2) {
 			return 1;
 		}
-		return fibo(a-1) + fibo(a-2);
+		return fibo(a - 1) + fibo(a - 2);
 	}
 
-	static class SumThread extends Thread{
-		
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		SumThread sumThread = new SumThread();
+		sumThread.setMainThread(Thread.currentThread());
+		sumThread.start();
+		LockSupport.park();
+
+		int result = sumThread.getResult();
+		System.out.println("异步计算结果：" + result);
+		System.out.println("计算耗时：" + (System.currentTimeMillis() - start) + "  ms");
+	}
+
+	static class SumThread extends Thread {
+
 		private Thread mainThread;
 
 		private Integer result;
@@ -40,18 +53,6 @@ public class AsyncRun07 {
 			result = sum();
 			LockSupport.unpark(getMainThread());
 		}
-	}
-
-	public static void main(String[] args) throws Exception{
-		long start = System.currentTimeMillis();
-		SumThread sumThread = new SumThread();
-		sumThread.setMainThread(Thread.currentThread());
-		sumThread.start();
-		LockSupport.park();
-		
-		int result = sumThread.getResult();
-		System.out.println("异步计算结果："+result);
-		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
 	}
 
 }
