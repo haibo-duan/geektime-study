6c3a71f7833e60d864e40d12aaa93b705027bdcd
haibo-duan <dhaibo1986@live.cn>
2021-08-25 11:41:02 +0800

    通过使用CountDownLatch来实现，同时格式化代码

--- src/main/java/com/dhb/gts/javacourse/week4/AsyncRun06.java
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun06.java
@@ -2,22 +2,37 @@ package com.dhb.gts.javacourse.week4;
 
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * 使用CountDownLatch来实现
+ */
 public class AsyncRun06 {
-	
+
 	private static final CountDownLatch latch = new CountDownLatch(1);
-	
+
 	private static int sum() {
 		return fibo(36);
 	}
 
 	private static int fibo(int a) {
-		if(a<2){
+		if (a < 2) {
 			return 1;
 		}
-		return fibo(a-1) + fibo(a-2);
+		return fibo(a - 1) + fibo(a - 2);
 	}
 
-	static class SumThread extends Thread{
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		SumThread sumThread = new SumThread();
+
+		sumThread.start();
+		latch.await();
+
+		int result = sumThread.getResult();
+		System.out.println("异步计算结果：" + result);
+		System.out.println("计算耗时：" + (System.currentTimeMillis() - start) + "  ms");
+	}
+
+	static class SumThread extends Thread {
 
 		private Integer result;
 
@@ -30,18 +45,6 @@ public class AsyncRun06 {
 			result = sum();
 			latch.countDown();
 		}
-	}
-
-	public static void main(String[] args) throws Exception{
-		long start = System.currentTimeMillis();
-		SumThread sumThread = new SumThread();
-
-		sumThread.start();
-		latch.await();
-		
-		int result = sumThread.getResult();
-		System.out.println("异步计算结果："+result);
-		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
 	}
 
 }
