426fd2e2d496decfa77925f5a57362b8833d07bb
haibo-duan <dhaibo1986@live.cn>
2021-08-25 11:27:16 +0800

    利用synchronized 确保计算线程先拿到lock 之后让main线程阻塞,同实格式化代码，增加注释

--- src/main/java/com/dhb/gts/javacourse/week4/AsyncRun04.java
+++ src/main/java/com/dhb/gts/javacourse/week4/AsyncRun04.java
@@ -1,21 +1,40 @@
 package com.dhb.gts.javacourse.week4;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 利用synchronized 确保计算线程先拿到lock 之后让main线程阻塞
+ */
 public class AsyncRun04 {
-	
+
 	private static final Object lock = new Object();
-	
+
 	private static int sum() {
 		return fibo(36);
 	}
 
 	private static int fibo(int a) {
-		if(a<2){
+		if (a < 2) {
 			return 1;
 		}
-		return fibo(a-1) + fibo(a-2);
+		return fibo(a - 1) + fibo(a - 2);
 	}
 
-	static class SumThread extends Thread{
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		SumThread sumThread = new SumThread();
+		sumThread.start();
+		
+		TimeUnit.MILLISECONDS.sleep(1);
+		synchronized (lock) {
+
+		}
+		int result = sumThread.getResult();
+		System.out.println("异步计算结果：" + result);
+		System.out.println("计算耗时：" + (System.currentTimeMillis() - start) + "  ms");
+	}
+
+	static class SumThread extends Thread {
 
 		private Integer result;
 
@@ -30,22 +49,4 @@ public class AsyncRun04 {
 			}
 		}
 	}
-
-	public static void main(String[] args) throws Exception{
-		long start = System.currentTimeMillis();
-		SumThread sumThread = new SumThread();
-
-		sumThread.start();
-
-		while (sumThread.isAlive()){
-			synchronized (lock) {
-
-			}
-		}
-		
-		int result = sumThread.getResult();
-		System.out.println("异步计算结果："+result);
-		System.out.println("计算耗时："+(System.currentTimeMillis() - start) +"  ms");
-	}
-
 }
