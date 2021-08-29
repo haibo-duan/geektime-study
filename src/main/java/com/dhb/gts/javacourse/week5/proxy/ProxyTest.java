8b05e28fcaf48d926912fbbf8ba65d1c478aa348
haibo-duan <dhaibo1986@live.cn>
2021-08-29 23:36:36 +0800

    测试jdk自带的动态代理方法

--- src/main/java/com/dhb/gts/javacourse/week5/proxy/ProxyTest.java
+++ src/main/java/com/dhb/gts/javacourse/week5/proxy/ProxyTest.java
@@ -1,16 +1,26 @@
 package com.dhb.gts.javacourse.week5.proxy;
 
+import java.lang.reflect.Proxy;
+
 public class ProxyTest {
 
 	public static void main(String[] args) {
 		testStaticProxy();
-		
+		testDynamicProxy();
 	}
 	
 	private static void testStaticProxy() {
 		Target target = new TargetImpl();
 		StaticProxy proxy = new StaticProxy(target);
 		String result = proxy.execute();
+		System.out.println(result);
+	}
+	
+	private static void testDynamicProxy() {
+		Target target = new TargetImpl();
+		DynamicProxy proxy = new DynamicProxy(target);
+		Target proxySubject = (Target)Proxy.newProxyInstance(TargetImpl.class.getClassLoader(),TargetImpl.class.getInterfaces(),proxy);
+		String result = proxySubject.execute();
 		System.out.println(result);
 	}
 }
