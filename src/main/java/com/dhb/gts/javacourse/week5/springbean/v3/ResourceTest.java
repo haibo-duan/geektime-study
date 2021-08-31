486acd5e2f2b4cd69a17e9ddb1f9b86729b79d50
haibo-duan <dhaibo1986@live.cn>
2021-08-31 18:55:13 +0800

    @Resource测试

---
+++ src/main/java/com/dhb/gts/javacourse/week5/springbean/v3/ResourceTest.java
@@ -0,0 +1,15 @@
+package com.dhb.gts.javacourse.week5.springbean.v3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ResourceTest {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("app-v3.xml");
+		UserRole userRole = (UserRole) context.getBean("userRole");
+		System.out.println(userRole.toString());
+		UserRole1 userRole1 = (UserRole1) context.getBean("userRole1");
+		System.out.println(userRole1.toString());
+	}
+}
