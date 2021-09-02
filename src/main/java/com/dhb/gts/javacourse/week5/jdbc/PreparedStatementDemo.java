02b5d8313596e53b250ad9282afe5c0508833f3a
haibo-duan <dhaibo1986@live.cn>
2021-09-02 20:13:58 +0800

    PreparedStatement的insert和delete方法

--- src/main/java/com/dhb/gts/javacourse/week5/jdbc/PreparedStatementDemo.java
+++ src/main/java/com/dhb/gts/javacourse/week5/jdbc/PreparedStatementDemo.java
@@ -6,12 +6,47 @@ import java.sql.ResultSet;
 
 public class PreparedStatementDemo {
 
-	public static void main(String[] args) {
-		updateRecordsToDbUsersTable();
+	public static void main(String[] args) throws Exception{
+		deleteRecordsFromDbUsersTable();
 	}
 	
+	
+	private static void deleteRecordsFromDbUsersTable() throws Exception{
+		Connection conn = MysqlConnection.getInstance().getCOnnection();
+		PreparedStatement ps = null;
+		String username = "sd";
+		try {
+			String sql = "delete from users where username = ? ;";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1,username);
+			ps.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(null != ps) {
+				ps.close();
+			}
+		}
+	}
 
-	private static void updateRecordsToDbUsersTable() {
+	private static void insertRecordToDbUsersTable() throws Exception{
+		Connection conn = MysqlConnection.getInstance().getCOnnection();
+		PreparedStatement ps = null;
+		try {
+			String sql = "insert users(username,password) values(\"sd\",\"dgrgdgdgd\")";
+			ps = conn.prepareStatement(sql);
+			ps.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(null != ps) {
+				ps.close();
+			}
+		}
+		
+	}
+	
+	private static void updateRecordsToDbUsersTable() throws Exception{
 		Connection conn = MysqlConnection.getInstance().getCOnnection();
 		PreparedStatement ps = null;
 		String id = "12";
@@ -24,11 +59,15 @@ public class PreparedStatementDemo {
 			System.out.println("update 语句执行影响行数："+result);
 		}catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			if(null != ps) {
+				ps.close();
+			}
 		}
 	}
 	
 	
-	private static void selectRecordsFromDbUsersTable() {
+	private static void selectRecordsFromDbUsersTable() throws Exception{
 		Connection conn = MysqlConnection.getInstance().getCOnnection();
 		PreparedStatement ps = null;
 		String uname = "james";
@@ -48,6 +87,10 @@ public class PreparedStatementDemo {
 			}
 		}catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			if(null != ps) {
+				ps.close();
+			}
 		}
 	}
 }
