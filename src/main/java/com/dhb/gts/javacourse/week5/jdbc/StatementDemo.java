139e8417b490433de2097d021c364bfd054e051d
haibo-duan <dhaibo1986@live.cn>
2021-09-02 19:32:24 +0800

    Statement测试其insert和select方法

---
+++ src/main/java/com/dhb/gts/javacourse/week5/jdbc/StatementDemo.java
@@ -0,0 +1,67 @@
+package com.dhb.gts.javacourse.week5.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StatementDemo {
+
+	public static void main(String[] args) throws Exception {
+		insertRecordToUsersTable();
+		selectRecordsFromDbUsersTable();
+	}
+
+	/**
+	 * 插入数据
+	 * @throws Exception
+	 */
+	public static void insertRecordToUsersTable() throws Exception {
+		Connection conn = MysqlConnection.getInstance().getCOnnection();
+		Statement statement = null;
+		String sql = "insert into users(id,username,password) values(12,\"dffd\",\"sddfef\")";
+		try {
+			statement = conn.createStatement();
+			boolean result = statement.execute(sql);
+			System.out.println(result);
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			if (null != statement) {
+				statement.close();
+			}
+			if (null != conn) {
+				conn.close();
+			}
+		}
+	}
+
+	/**
+	 * 查询数据
+	 * @throws Exception
+	 */
+	public static void selectRecordsFromDbUsersTable() throws Exception {
+		Connection conn = MysqlConnection.getInstance().getCOnnection();
+		Statement statement = null;
+		String sql = "select id,username,password from users;";
+		try {
+			statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			System.out.println(rs.getFetchSize());
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String username = rs.getString("username");
+				System.out.println("查询到数据： id [" + id + "] username [" + username + "]");
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			if (null != statement) {
+				statement.close();
+			}
+			if (null != conn) {
+				conn.close();
+			}
+		}
+	}
+}
