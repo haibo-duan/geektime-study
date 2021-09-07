61251894d99f7bf8998a0788bde70758c535753e
haibo-duan <dhaibo1986@live.cn>
2021-09-07 16:21:49 +0800

    重新生成代码

---
+++ src/main/java/com/dhb/gts/javacourse/week5/entity/Users.java
@@ -0,0 +1,88 @@
+package com.dhb.gts.javacourse.week5.entity;
+
+import java.util.Date;
+
+public class Users {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Date createdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Users other = (Users) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()))
+            && (this.getCreatedate() == null ? other.getCreatedate() == null : this.getCreatedate().equals(other.getCreatedate()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        result = prime * result + ((getCreatedate() == null) ? 0 : getCreatedate().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", createdate=").append(createdate);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/dhb/gts/javacourse/week5/mapper/UsersMapper.java
@@ -0,0 +1,49 @@
+package com.dhb.gts.javacourse.week5.mapper;
+
+import com.dhb.gts.javacourse.week5.entity.Users;
+import com.dhb.gts.javacourse.week5.entity.UsersExample;
+import com.dhb.gts.javacourse.week5.typehandler.MyDateTypeHandler;
+import java.util.List;
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.type.JdbcType;
+
+public interface UsersMapper {
+    @SelectProvider(type=UsersSqlProvider.class, method="countByExample")
+    long countByExample(UsersExample example);
+
+    @DeleteProvider(type=UsersSqlProvider.class, method="deleteByExample")
+    int deleteByExample(UsersExample example);
+
+    @Insert({
+        "insert into users (id, username, ",
+        "`password`, createDate)",
+        "values (#{id,jdbcType=INTEGER}, #{username,jdbcType=VARCHAR}, ",
+        "#{password,jdbcType=VARCHAR}, #{createdate,jdbcType=VARCHAR,typeHandler=com.dhb.gts.javacourse.week5.typehandler.MyDateTypeHandler})"
+    })
+    int insert(Users record);
+
+    @InsertProvider(type=UsersSqlProvider.class, method="insertSelective")
+    int insertSelective(Users record);
+
+    @SelectProvider(type=UsersSqlProvider.class, method="selectByExample")
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER),
+        @Result(column="username", property="username", jdbcType=JdbcType.VARCHAR),
+        @Result(column="password", property="password", jdbcType=JdbcType.VARCHAR),
+        @Result(column="createDate", property="createdate", typeHandler=MyDateTypeHandler.class, jdbcType=JdbcType.VARCHAR)
+    })
+    List<Users> selectByExample(UsersExample example);
+
+    @UpdateProvider(type=UsersSqlProvider.class, method="updateByExampleSelective")
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    @UpdateProvider(type=UsersSqlProvider.class, method="updateByExample")
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+}
