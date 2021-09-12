c26111905bc2e836269d0eacbca428bcdae60426
haibo-duan <dhaibo1986@live.cn>
2021-09-12 16:24:13 +0800

    增加批量插入数据API

--- src/main/java/com/dhb/gts/javacourse/week6/mysqltest/RandomInsertOrderTable.java
+++ src/main/java/com/dhb/gts/javacourse/week6/mysqltest/RandomInsertOrderTable.java
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @Slf4j
@@ -28,30 +30,51 @@ public class RandomInsertOrderTable {
 
 	@Autowired
 	OrderSummaryMapper orderSummaryMapper;
-	
+
 	@Autowired
 	OrderDetailMapper orderDetailMapper;
 
 
 	@RequestMapping("/randomInsertOrderTable")
-	public String randomInsertOrderTable(String number) {
-		log.info("randomInsertOrderTable  insert records number is [" + number + "]");
+	public String randomInsertOrderTable(String total, String batch) {
+		log.info("randomInsertOrderTable  insert records total is [" + total + "]");
 
-		int batchNumber = 0;
+		int totalNumbers = 0;
+		int batchSize = 0;
 		try {
-			batchNumber = Integer.parseInt(number);
+			totalNumbers = Integer.parseInt(total);
+			batchSize = Integer.parseInt(batch);
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
-		randomBatchInsertOder(batchNumber);
+		randomBatchInsertOder(totalNumbers, batchSize);
 		return "success";
 	}
 
-	private void randomBatchInsertOder(int number) {
+	private void randomBatchInsertOder(int totalSize, int batchSize) {
 		int maxOrderNo = getMaxOderNo();
 		log.info("maxOrderNo is [" + maxOrderNo + "]!");
-		int orderId = maxOrderNo +1;
-		insertOrder(orderId);
+		log.info("数据批流插入开始...");
+		long start = System.currentTimeMillis();
+		int orderId = maxOrderNo + 1;
+		int count = 0;
+		if (batchSize != 0 && totalSize != 0) {
+			count = totalSize / batchSize;
+		}
+		for (int i = 0; i <= count; i++) {
+			log.info("批次[" + i + "]处理开始...");
+			List<OrderSummaryEntity> summarys = buildBatchOrderSummary(orderId, batchSize);
+			List<OrderDetailEntity> detials = buildBatcOrderDetail(orderId, batchSize);
+			batchInsert(summarys, detials);
+			orderId += batchSize;
+			log.info("批次[" + i + "]处理完毕...");
+		}
+		log.info("批量插入数据 totalSize [" + totalSize + "]... 共耗时[" + (System.currentTimeMillis() - start) + "]");
+	}
+
+	private void batchInsert(List<OrderSummaryEntity> summarys, List<OrderDetailEntity> detials) {
+		orderSummaryMapper.insertBatch(summarys);
+		orderDetailMapper.insertBatch(detials);
 	}
 
 	private int getMaxOderNo() {
@@ -66,20 +89,6 @@ public class RandomInsertOrderTable {
 	}
 
 	private void insertOrder(int orderId) {
-		OrderSummaryEntity orderSummaryEntity = new OrderSummaryEntity()
-				.setOrderNo(orderId)
-				.setConsigneeAddress("北京市朝阳区西坝河")
-				.setConsigneeName("张三")
-				.setConsigneePhone("13888888888")
-				.setCustomerId(10001)
-				.setExpressNo("345454622324")
-				.setExpressComp("顺丰")
-				.setPaymentMethod(1)
-				.setPaymentMoney(new BigDecimal(56.00))
-				.setOrderAmount(new BigDecimal(56.00))
-				.setIsValidate(1);
-		orderSummaryMapper.insert(orderSummaryEntity);
-
 		OrderDetailEntity orderDetailEntity1 = new OrderDetailEntity()
 				.setOrderId(orderId)
 				.setProductId(10002)
@@ -100,5 +109,68 @@ public class RandomInsertOrderTable {
 				.setAverageCost(new BigDecimal(6))
 				.setIsValidate(1);
 		orderDetailMapper.insert(orderDetailEntity2);
+	}
+
+	private List<OrderSummaryEntity> buildBatchOrderSummary(int orderId, int batchNumber) {
+		List<OrderSummaryEntity> orderSummarys = new ArrayList<>();
+		for (int i = 0; i < batchNumber; i++) {
+			orderId += 1;
+			orderSummarys.add(buildOrderSummary(orderId));
+		}
+		return orderSummarys;
+	}
+
+	private List<OrderDetailEntity> buildBatcOrderDetail(int orderId, int batchNumber) {
+		List<OrderDetailEntity> orderSummarys = new ArrayList<>();
+		for (int i = 0; i < batchNumber; i++) {
+			orderId += 1;
+			orderSummarys.add(buildOrderDetail1(orderId));
+			orderSummarys.add(buildOrderDetail2(orderId));
+		}
+		return orderSummarys;
+	}
+
+	private OrderSummaryEntity buildOrderSummary(int orderId) {
+		Random random = new Random(System.currentTimeMillis());
+		int expressNo = random.nextInt(10000000) + 10000000;
+		OrderSummaryEntity orderSummaryEntity = new OrderSummaryEntity()
+				.setOrderNo(orderId)
+				.setConsigneeAddress("北京市朝阳区西坝河")
+				.setConsigneeName("张三")
+				.setConsigneePhone("13888888888")
+				.setCustomerId(10001)
+				.setExpressNo("1001" + expressNo)
+				.setExpressComp("顺丰")
+				.setPaymentMethod(1)
+				.setPaymentMoney(new BigDecimal(56.00))
+				.setOrderAmount(new BigDecimal(56.00))
+				.setIsValidate(1);
+		return orderSummaryEntity;
+	}
+
+	private OrderDetailEntity buildOrderDetail1(int orderId) {
+		OrderDetailEntity orderDetailEntity1 = new OrderDetailEntity()
+				.setOrderId(orderId)
+				.setProductId(10002)
+				.setProductName("手机钢化膜")
+				.setPrice(new BigDecimal(14))
+				.setNumbers(2)
+				.setTotal(new BigDecimal(28))
+				.setAverageCost(new BigDecimal(12))
+				.setIsValidate(1);
+		return orderDetailEntity1;
+	}
+
+	private OrderDetailEntity buildOrderDetail2(int orderId) {
+		OrderDetailEntity orderDetailEntity2 = new OrderDetailEntity()
+				.setOrderId(orderId)
+				.setProductId(10003)
+				.setProductName("手机壳")
+				.setPrice(new BigDecimal(7))
+				.setNumbers(4)
+				.setTotal(new BigDecimal(28))
+				.setAverageCost(new BigDecimal(6))
+				.setIsValidate(1);
+		return orderDetailEntity2;
 	}
 }
