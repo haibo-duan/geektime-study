52638f3b8c2dbe6c080ec3ac88a3b304e471b965
haibo-duan <dhaibo1986@live.cn>
2021-09-13 17:37:20 +0800

    修复使用dao的时候出现的bing Exception异常。 之前的代码存在一个bug,MapperScan注解里面只能对Mapper进行扫描。而之前的代码扫描了dao,虽然能加载到spring中，但是使用起来会出错。dao层的代码，要么Starter启动的时候扫描所在目录的下层目录。要么通过 @SpringBootApplication(scanBasePackages= {}) 进行指定。

--- src/main/java/com/dhb/gts/javacourse/week6/mysqltest/Starter.java
+++ src/main/java/com/dhb/gts/javacourse/week6/mysqltest/Starter.java
@@ -4,8 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@MapperScan(basePackages = {"com.dhb.gts.javacourse.fluent"})
+@SpringBootApplication(scanBasePackages  = {"com.dhb.gts.javacourse.fluent.dao","com.dhb.gts.javacourse.week6.mysqltest"} )
+@MapperScan(basePackages = {"com.dhb.gts.javacourse.fluent.mapper"})
 public class Starter {
 
 	public static void main(String[] args) {
