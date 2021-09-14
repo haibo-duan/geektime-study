81d8f3ae2d6d2ed504eff8e2019a54a783174f66
haibo-duan <dhaibo1986@live.cn>
2021-09-14 15:31:30 +0800

    增加两个随机修改的方法 randomOneModifyByKey 和 randomOneModifyByNo 分别通过主键和非主键对数据进行修改

---
+++ src/main/java/com/dhb/gts/javacourse/week6/mysqltest/UpdateOrderTable.java
@@ -0,0 +1,64 @@
+package com.dhb.gts.javacourse.week6.mysqltest;
+
+import com.alibaba.fastjson.JSON;
+import com.dhb.gts.javacourse.fluent.dao.intf.OrderSummaryDao;
+import com.dhb.gts.javacourse.fluent.entity.OrderSummaryEntity;
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@Slf4j
+public class UpdateOrderTable {
+
+	@Autowired
+	OrderSummaryDao orderSummaryDao;
+
+	@RequestMapping("/randomOneModifyByKey")
+	public String randomModifyByKey() {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		Random random = new Random(System.currentTimeMillis());
+		int maxOrderId = orderSummaryDao.selectMaxOrderId();
+		int orderId = maxOrderId;
+		if(maxOrderId > 10000) {
+			orderId = random.nextInt(maxOrderId-10000)+5000;
+		}else {
+			orderId = random.nextInt(maxOrderId);
+		}
+		OrderSummaryEntity entity = new OrderSummaryEntity()
+				.setOrderId(orderId)
+						.setConsigneeAddress("北京市丰台区方庄南路19号")
+								.setPaymentMethod(2);
+		orderSummaryDao.saveOrUpdate(entity);
+		stopwatch.stop();
+		System.out.println("通过key修改 key is["+entity.getOrderId()+"] cost is ：" + stopwatch);
+		return "success";
+	}
+
+	@RequestMapping("/randomOneModifyByNo")
+	public String randomOneModifyByNo() {
+		Stopwatch stopwatch = Stopwatch.createStarted();
+		Random random = new Random(System.currentTimeMillis());
+		int maxOrderNo = orderSummaryDao.selectMaxOrderNo();
+		int orderNo = maxOrderNo;
+		if(maxOrderNo > 10000) {
+			orderNo = random.nextInt(maxOrderNo-10000)+5000;
+		}else {
+			orderNo = random.nextInt(maxOrderNo);
+		}
+		OrderSummaryEntity where = new OrderSummaryEntity()
+				.setOrderNo(orderNo);
+		OrderSummaryEntity 	update = new OrderSummaryEntity()
+				.setConsigneeAddress("北京市丰台区方庄南路19号")
+				.setPaymentMethod(2);
+		orderSummaryDao.updateBy(update,where);
+		stopwatch.stop();
+		System.out.println("通过key修改 key is["+where.getOrderId()+"] cost is ：" + stopwatch);
+		return "success";
+	}
+	
+}
