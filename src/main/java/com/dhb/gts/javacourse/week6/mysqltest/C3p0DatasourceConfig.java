f06946a0bdae169336477eea4f680c9c07d2fe84
haibo-duan <dhaibo1986@live.cn>
2021-09-14 10:43:59 +0800

    采用c3p0的连接池进行测试

---
+++ src/main/java/com/dhb/gts/javacourse/week6/mysqltest/C3p0DatasourceConfig.java
@@ -0,0 +1,21 @@
+package com.dhb.gts.javacourse.week6.mysqltest;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class C3p0DatasourceConfig {
+
+	@Bean(name = "dataSource")
+	@Qualifier(value = "dataSource")
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		       return DataSourceBuilder.create().type(com.mchange.v2.c3p0.ComboPooledDataSource.class).build();
+	}
+	
+}
