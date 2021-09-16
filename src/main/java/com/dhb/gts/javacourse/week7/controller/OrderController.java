1c766e59c35e4365c271c14404a1e95db618b8ab
haibo-duan <dhaibo1986@live.cn>
2021-09-16 21:11:42 +0800

    修改包名

--- src/main/java/com/dhb/gts/javacourse/week7/controller/OrderController.java
+++ src/main/java/com/dhb/gts/javacourse/week7/controller/OrderController.java
@@ -1,4 +1,4 @@
-package com.dhb.gts.javacourse.week7.batchinsert;
+package com.dhb.gts.javacourse.week7.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.dhb.gts.javacourse.fluent.entity.OrderSummaryEntity;
