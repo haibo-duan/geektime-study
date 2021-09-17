eda3021f21d50c223d436cbefbe0817c37809fb8
haibo-duan <dhaibo1986@live.cn>
2021-09-17 14:03:06 +0800

    将线程池的参数配置到applications.yml中，通过@Value注入

--- src/main/java/com/dhb/gts/javacourse/week7/BeanConfig.java
+++ src/main/java/com/dhb/gts/javacourse/week7/BeanConfig.java
@@ -1,6 +1,8 @@
 package com.dhb.gts.javacourse.week7;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -11,25 +13,39 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
+@Slf4j
 public class BeanConfig {
+
+	@Value("${async.executor.thread.core_pool_size}")
+	private int corePoolSize;
+	@Value("${async.executor.thread.max_pool_size}")
+	private int maxPoolSize;
+	@Value("${async.executor.thread.queue_capacity}")
+	private int queueCapacity;
+	@Value("${async.executor.thread.name.prefix}")
+	private String namePrefix;
+	@Value("${async.executor.thread.keep_alive_seconds}")
+	private int keepAliveSeconds;
 
 	@Bean
 	public TaskExecutor taskExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		// 设置核心线程数
-		executor.setCorePoolSize(5);
+		executor.setCorePoolSize(corePoolSize);
 		// 设置最大线程数
-		executor.setMaxPoolSize(10);
+		executor.setMaxPoolSize(maxPoolSize);
 		// 设置队列容量
-		executor.setQueueCapacity(20);
+		executor.setQueueCapacity(queueCapacity);
 		// 设置线程活跃时间（秒）
-		executor.setKeepAliveSeconds(60);
+		executor.setKeepAliveSeconds(keepAliveSeconds);
 		// 设置默认线程名称
-		executor.setThreadNamePrefix("taskpool-");
+		executor.setThreadNamePrefix(namePrefix);
 		// 设置拒绝策略
 		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
 		// 等待所有任务结束后再关闭线程池
 		executor.setWaitForTasksToCompleteOnShutdown(true);
+		log.info("创建一个线程池 corePoolSize is [" + corePoolSize + "] maxPoolSize is [" + maxPoolSize + "] queueCapacity is [" + queueCapacity +
+				"] keepAliveSeconds is [" + keepAliveSeconds + "] namePrefix is [" + namePrefix + "].");
 		return executor;
 	}
 }
