82ad21786acca07f0f644c4d1d6c3e18f04845a0
haibo-duan <dhaibo1986@live.cn>
2021-09-18 20:21:27 +0800

    启动器import数据源注册类,并开启切面

--- src/main/java/com/dhb/gts/javacourse/week7/Starter.java
+++ src/main/java/com/dhb/gts/javacourse/week7/Starter.java
@@ -1,11 +1,16 @@
 package com.dhb.gts.javacourse.week7;
 
+import com.dhb.gts.javacourse.week7.dynamic.DynamicDataSourceRegister;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages  = {"com.dhb.gts.javacourse.fluent.dao","com.dhb.gts.javacourse.week7"} )
 @MapperScan(basePackages = {"com.dhb.gts.javacourse.fluent.mapper"})
+@Import({DynamicDataSourceRegister.class})
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class Starter {
 
 	public static void main(String[] args) {
