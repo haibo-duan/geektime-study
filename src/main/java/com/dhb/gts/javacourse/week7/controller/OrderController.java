943005e45d82a070af653d578fcb3604df41ff7a
haibo-duan <dhaibo1986@live.cn>
2021-09-19 22:12:40 +0800

    增加通线程池多线程异步方式批次插入，可以将insert性能提升一倍以上

--- src/main/java/com/dhb/gts/javacourse/week7/controller/OrderController.java
+++ src/main/java/com/dhb/gts/javacourse/week7/controller/OrderController.java
@@ -4,12 +4,16 @@ import com.alibaba.fastjson.JSON;
 import com.dhb.gts.javacourse.fluent.entity.OrderSummaryEntity;
 import com.dhb.gts.javacourse.week7.service.OrderService;
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
 
 @RestController
 @Slf4j
@@ -33,6 +37,22 @@ public class OrderController {
 		orderService.randomBatchInsertOder(totalNumbers, batchSize);
 		return "success";
 	}
+
+	@RequestMapping("/asyncRandomInsertOrderTable")
+	public String asyncRandomInsertOrderTable(String total, String batch) {
+		log.info("randomInsertOrderTable  insert records total is [" + total + "] .........");
+		int totalNumbers = 0;
+		int batchSize = 0;
+		try {
+			totalNumbers = Integer.parseInt(total);
+			batchSize = Integer.parseInt(batch);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		concurrentRandomBatchInsertOrder(totalNumbers, batchSize);
+		return "success";
+	}
+	
 
 	@RequestMapping("/randomInsertOneOrder")
 	public String randomInsertOneOrder() {
@@ -79,8 +99,43 @@ public class OrderController {
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		stopwatch.stop();
 		log.info("通过key查询，走索引耗时：" + stopwatch);
 		return JSON.toJSONString(entity);
+	}
+
+	private void concurrentRandomBatchInsertOrder(int totalSize, int batchSize) {
+		int maxOrderNo = orderService.getMaxOderNo();
+		List<Integer> costs = Lists.newArrayList();
+		log.info("maxOrderNo is [" + maxOrderNo + "]!");
+		log.info("数据并行批次插入开始...");
+		long start = System.currentTimeMillis();
+		int count = 0;
+		if (batchSize != 0 && totalSize != 0) {
+			count = totalSize / batchSize;
+		}
+		List<Future<Integer>> results = new ArrayList<>();
+		for (int i = 0; i <= count; i++) {
+			log.info("批次[" + i + "]处理开始...");
+			results.add(orderService.ansyncInsertOrder(batchSize,maxOrderNo));
+			maxOrderNo += batchSize;
+		}
+
+		for(int i=0;i<results.size();i++){
+			try {
+				int cost = results.get(i).get();
+				costs.add(cost);
+				log.info("批次[" + i + "]处理完毕,耗时：" + cost + " ms");
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		long max = costs.stream().mapToLong(Integer::intValue).max().getAsLong();
+		long min = costs.stream().mapToLong(Integer::intValue).min().getAsLong();
+		double average = costs.stream().mapToLong(Integer::intValue).average().getAsDouble();
+		log.info("总计插入批次共" + costs.size() + "次，其中，最大耗时:" + max + "ms 最小耗时：" + min + "ms 平均耗时:" + average + "ms");
+		log.info("批量插入数据 totalSize [" + totalSize + "]... 共耗时[" + (System.currentTimeMillis() - start) + "] ms");
 	}
 }
