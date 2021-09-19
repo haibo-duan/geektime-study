1ee9d61a4b029f05bc005bfaf38d1267c36fc98d
haibo-duan <dhaibo1986@live.cn>
2021-09-19 23:41:02 +0800

    需要调整下if判断顺序，调试成功。这个方法未来可以扩展为自定义的负载均衡算法。

--- src/main/java/com/dhb/gts/javacourse/week7/dynamic/DynamicDataSourceAspect.java
+++ src/main/java/com/dhb/gts/javacourse/week7/dynamic/DynamicDataSourceAspect.java
@@ -21,20 +21,23 @@ public class DynamicDataSourceAspect {
 	@Before("@annotation(ds)")
 	public void changeDataSource(JoinPoint point, TargetDataSource ds) throws Throwable {
 		String dsId = ds.name();
-		if (!DynamicDataSourceContextHolder.containsDataSource(dsId)) {
-			log.error("数据源[{}]不存在，使用默认数据源 > {}", ds.name(), point.getSignature());
-		}else if("slave".equals(dsId)){
+		if("slave".equals(dsId)){
 			log.info("如果传入为slave标识，则将在所有的slave数据库中进行负载均衡");
 			//首先通过slaveLoadBalance 方法获得一个数据源name
 			String dsName = slaveLoadBalance();
 			//由于master与slave数据源都在这个数组里面，因此可能存在拿到master数据源的情况，此处如果没有拿到slave数据源将会重试
 			for(int i=0;i<DynamicDataSourceContextHolder.dataSourceIds.size();i++) {
 				if(dsName.startsWith("slave")){
+					log.info("Use DataSource : {} > {}", dsName, point.getSignature());
 					DynamicDataSourceContextHolder.setDataSourceType(dsName);
 					break;
 				}
+				dsName = slaveLoadBalance();
 			}
+			log.error("尝试多次仍然没有获取到slave相关的数据源，将使用默认数据源 > {}",  point.getSignature());
 			//如果尝试完成仍然没有拿到合适的数据源，则将通过默认数据源
+		}else if (!DynamicDataSourceContextHolder.containsDataSource(dsId)) {
+			log.error("数据源[{}]不存在，使用默认数据源 > {}", ds.name(), point.getSignature());
 		}else {
 			log.info("Use DataSource : {} > {}", dsId, point.getSignature());
 			DynamicDataSourceContextHolder.setDataSourceType(dsId);
