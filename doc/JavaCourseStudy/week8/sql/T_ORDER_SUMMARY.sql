58200577a87b67bcd1989a85ef871b476df657ef
haibo-duan <dhaibo1986@live.cn>
2021-09-23 20:29:06 +0800

    修改逻辑表结构为小写

--- doc/JavaCourseStudy/week8/sql/T_ORDER_SUMMARY.sql
+++ doc/JavaCourseStudy/week8/sql/T_ORDER_SUMMARY.sql
@@ -1,23 +1,23 @@
 /*
 * 逻辑表
  */
-drop table if exists `T_ORDER_SUMMARY`;
-CREATE TABLE T_ORDER_SUMMARY  (
-                                    ORDER_ID         	bigint  COMMENT '订单ID'  NOT NULL,
-                                    ORDER_NO         	int(11) COMMENT '订单编号'  NOT NULL,
-                                    CUSTOMER_ID      	int(11) COMMENT '下单用户ID'  NOT NULL,
-                                    PAYMENT_METHOD   	tinyint(4) COMMENT '支付方式：1现金，2余额，3网银，4支付宝，5微信'  NOT NULL,
-                                    ORDER_AMOUNT     	int(11) COMMENT '订单汇总金额 单位 分  此处为int,比bigint节约4个字节'  NOT NULL,
-                                    PAYMENT_MONEY    	int(11) COMMENT '支付金额  单位 分  此处为int,比bigint节约4个字节'  NOT NULL,
-                                    CONSIGNEE_NAME   	varchar(50) COMMENT '收货人姓名'  NOT NULL,
-                                    CONSIGNEE_ADDRESS	varchar(100) COMMENT '收货人详细'  NOT NULL,
-                                    CONSIGNEE_PHONE  	varchar(30) COMMENT '收货人联系电话'  NOT NULL,
-                                    EXPRESS_COMP     	varchar(30) COMMENT '快递公司名称'  NOT NULL,
-                                    EXPRESS_NO       	varchar(50) COMMENT '快递单号'  NOT NULL,
-                                    CREATE_TIME      	timestamp COMMENT '创建时间'  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                    IS_VALIDATE      	tinyint(4) COMMENT '数据是否有效标识：1有效数据，2 无效数据'  NOT NULL DEFAULT '1',
-                                    UPDATE_TIME      	timestamp COMMENT '最新更新时间'  NOT NULL,
-                                    PRIMARY KEY(ORDER_ID)
+drop table if exists `t_order_summary`;
+create table t_order_summary  (
+                                    order_id         	bigint  comment '订单id'  not null,
+                                    order_no         	int(11) comment '订单编号'  not null,
+                                    customer_id      	int(11) comment '下单用户id'  not null,
+                                    payment_method   	tinyint(4) comment '支付方式：1现金，2余额，3网银，4支付宝，5微信'  not null,
+                                    order_amount     	int(11) comment '订单汇总金额 单位 分  此处为int,比bigint节约4个字节'  not null,
+                                    payment_money    	int(11) comment '支付金额  单位 分  此处为int,比bigint节约4个字节'  not null,
+                                    consignee_name   	varchar(50) comment '收货人姓名'  not null,
+                                    consignee_address	varchar(100) comment '收货人详细'  not null,
+                                    consignee_phone  	varchar(30) comment '收货人联系电话'  not null,
+                                    express_comp     	varchar(30) comment '快递公司名称'  not null,
+                                    express_no       	varchar(50) comment '快递单号'  not null,
+                                    create_time      	timestamp comment '创建时间'  not null default current_timestamp,
+                                    is_validate      	tinyint(4) comment '数据是否有效标识：1有效数据，2 无效数据'  not null default '1',
+                                    update_time      	timestamp comment '最新更新时间'  not null,
+                                    primary key(order_id)
 )
-    ENGINE = InnoDB
-COMMENT = '订单汇总信息表-逻辑表' ;
+    engine = innodb
+comment = '订单汇总信息表-逻辑表' ;
