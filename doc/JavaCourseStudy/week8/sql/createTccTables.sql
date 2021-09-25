c167d6a62226ea2e1dd20a135a2b7d3b68251e03
haibo-duan <dhaibo1986@live.cn>
2021-09-25 23:35:42 +0800

    Tcc事务3张日志表

---
+++ doc/JavaCourseStudy/week8/sql/createTccTables.sql
@@ -0,0 +1,17 @@
+drop table if exists `t_try_log`;
+CREATE TABLE `t_try_log` (
+                                 `tx_no` varchar(64) NOT NULL COMMENT '事务id',
+                                 `create_time` datetime DEFAULT NULL,
+                                 PRIMARY KEY (`tx_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists `t_confirm_log`;
+CREATE TABLE `t_confirm_log` (
+                                     `tx_no` varchar(64) NOT NULL COMMENT '事务id',
+                                     `create_time` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists `t_cancel_log`;
+CREATE TABLE `t_cancel_log` (
+                                    `tx_no` varchar(64) NOT NULL COMMENT '事务id',
+                                    `create_time` datetime DEFAULT NULL,
+                                    PRIMARY KEY (`tx_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
