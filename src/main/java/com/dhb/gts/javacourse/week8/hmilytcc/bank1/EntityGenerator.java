cf71340505af580e16be8015bc1f9dd76ac55804
haibo-duan <dhaibo1986@live.cn>
2021-09-26 00:20:20 +0800

    tcc相关表代码自动生成

---
+++ src/main/java/com/dhb/gts/javacourse/week8/hmilytcc/bank1/EntityGenerator.java
@@ -0,0 +1,50 @@
+package com.dhb.gts.javacourse.week8.hmilytcc.bank1;
+
+
+import cn.org.atool.generator.FileGenerator;
+import cn.org.atool.generator.annotation.Column;
+import cn.org.atool.generator.annotation.Table;
+import cn.org.atool.generator.annotation.Tables;
+import com.dhb.gts.javacourse.week5.typehandler.MyDateTypeHandler;
+
+import java.util.Date;
+
+public class EntityGenerator {
+
+	// 数据源 url
+	static final String url = "jdbc:mysql://192.168.161.114:3306/gts01?useSSL=false&autoReconnect=true&characterEncoding=UTF-8&serverTimezone=UTC";
+	// 数据库用户名
+	static final String username = "gts";
+	// 数据库密码
+	static final String password = "mysql";
+
+	public static void main(String[] args) {
+		// 引用配置类，build方法允许有多个配置类
+		FileGenerator.build(Empty.class);
+	}
+
+
+	@Tables(
+			// 设置数据库连接信息
+			url = url, username = username, password = password,driver = "com.mysql.cj.jdbc.Driver",
+			// 设置entity类生成src目录, 相对于 user.dir
+			srcDir = "src/gens/java",
+			// 设置entity类的package值
+			basePack = "com.dhb.gts.javacourse.fluent",
+			// 设置dao接口和实现的src目录, 相对于 user.dir
+			daoDir = "src/gens/java",
+			// 设置哪些表要生成Entity文件
+			tables = {
+					@Table(value = {"t_try_log"},
+							gmtCreated = "CREATE_TIME"),
+					@Table(value = {"t_confirm_log"},
+							gmtCreated = "CREATE_TIME"),
+					@Table(value = {"t_cancel_log"},
+							gmtCreated = "CREATE_TIME")
+			}
+	)
+	static class Empty {
+	}
+
+
+}
