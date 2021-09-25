03059ce2125d31829da36b81e82faf612ca9484f
haibo-duan <dhaibo1986@live.cn>
2021-09-25 23:53:43 +0800

    t_confirm_log表忘记增加PK

--- doc/JavaCourseStudy/week8/sql/createTccTables.sql
+++ doc/JavaCourseStudy/week8/sql/createTccTables.sql
@@ -7,7 +7,8 @@ CREATE TABLE `t_try_log` (
 drop table if exists `t_confirm_log`;
 CREATE TABLE `t_confirm_log` (
                                      `tx_no` varchar(64) NOT NULL COMMENT '事务id',
-                                     `create_time` datetime DEFAULT NULL
+                                     `create_time` datetime DEFAULT NULL,
+                                     PRIMARY KEY (`tx_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 drop table if exists `t_cancel_log`;
 CREATE TABLE `t_cancel_log` (
