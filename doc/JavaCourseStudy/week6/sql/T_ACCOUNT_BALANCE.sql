ae12bc0405a3d52fa568a4825570d9128c706380
haibo-duan <dhaibo1986@live.cn>
2021-09-26 00:19:06 +0800

    改为小写

--- doc/JavaCourseStudy/week6/sql/T_ACCOUNT_BALANCE.sql
+++ doc/JavaCourseStudy/week6/sql/T_ACCOUNT_BALANCE.sql
@@ -1,30 +1,30 @@
 /*
-Navicat MySQL Data Transfer
+navicat mysql data transfer
 
-Source Server         : 192.168.162.49(gts)
-Source Server Version : 50717
-Source Host           : 192.168.162.49:3306
-Source Database       : gts
+source server         : 192.168.162.49(gts)
+source server version : 50717
+source host           : 192.168.162.49:3306
+source database       : gts
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
+target server type    : mysql
+target server version : 50717
+file encoding         : 65001
 
-Date: 2021-09-10 15:04:52
+date: 2021-09-10 15:04:52
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+set foreign_key_checks=0;
 
 -- ----------------------------
--- Table structure for T_ACCOUNT_BALANCE
+-- table structure for t_account_balance
 -- ----------------------------
-DROP TABLE IF EXISTS `T_ACCOUNT_BALANCE`;
-CREATE TABLE `T_ACCOUNT_BALANCE` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键 自增列',
-  `CUSTOMER_ID` int(11) NOT NULL COMMENT '用户编号',
-  `BALANCE`  bigint NOT NULL COMMENT '客户余额 单位 分',
-  `CREATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '用户注册时间',
-  `IS_VALIDATE` tinyint(4) NOT NULL DEFAULT '1' COMMENT '数据是否有效标识：1有效数据，2 无效数据',
-  `UPDATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+drop table if exists `t_account_balance`;
+create table `t_account_balance` (
+  `id` int(11) not null auto_increment comment '主键 自增列',
+  `customer_id` int(11) not null comment '用户编号',
+  `balance`  bigint not null comment '客户余额 单位 分',
+  `create_time` timestamp not null default current_timestamp on update current_timestamp comment '用户注册时间',
+  `is_validate` tinyint(4) not null default '1' comment '数据是否有效标识：1有效数据，2 无效数据',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '最后修改时间',
+  primary key (`id`)
+) engine=innodb default charset=utf8;
