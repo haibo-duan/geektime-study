683d79dbecd2446586192b0fe222ea6ee4d3004f
haibo-duan <dhaibo1986@live.cn>
2021-09-25 23:37:12 +0800

    修改重构后的代码的扫描url

--- src/main/java/com/dhb/gts/javacourse/week8/ss/Starter.java
+++ src/main/java/com/dhb/gts/javacourse/week8/ss/Starter.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.dhb.gts.javacourse.fluent.dao", "com.dhb.gts.javacourse.week8"})
+@SpringBootApplication(scanBasePackages = {"com.dhb.gts.javacourse.fluent.dao", "com.dhb.gts.javacourse.week8.ss"})
 @MapperScan(basePackages = {"com.dhb.gts.javacourse.fluent.mapper"})
 public class Starter {
 
