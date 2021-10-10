6dcd7633510a19ce6fe32f289e2daf8fe5b36c5f
haibo-duan <dhaibo1986@live.cn>
2021-10-10 23:47:04 +0800

    建表语句

---
+++ doc/JavaCourseStudy/week9/project/sql/T_BANK_FREEZE.sql
@@ -0,0 +1,12 @@
+
+drop table if exists `t_bank_account`;
+create table t_bank_freeze (
+       id         	int(11) auto_increment comment '主键 自增列'  not null,
+       customer_id	int(11) comment '用户编号'  not null,
+       account_type tinyint(4) comment '账户类型：1 人民币账户，2 美元账户'  not null default '1',
+       amount    	bigint(20) comment '客户余额 单位 分'  not null,
+       create_time	timestamp comment '创建时间'  not null,
+       is_validate	tinyint(4) comment '数据是否有效标识：1有效数据，2 无效数据'  not null default '1',
+       update_time	timestamp comment '最后修改时间'  not null,
+       primary key(id)
+) engine=innodb default charset=utf8  auto_increment=1;
