bcd9762a7f0f3cf2e1f52001d1e632da22e70d9e
haibo-duan <dhaibo1986@live.cn>
2021-10-19 20:35:55 +0800

    测试mybatis的缓存的project 自动生成了mybatis的orm

---
+++ doc/JavaCourseStudy/week11/project/mybatis-cache-demo/src/main/java/com/dhb/mybatis/cache/orm/dao/intf/BankAccountDao.java
@@ -0,0 +1,14 @@
+package com.dhb.mybatis.cache.orm.dao.intf;
+
+import cn.org.atool.fluent.mybatis.base.IBaseDao;
+import com.dhb.mybatis.cache.orm.entity.BankAccountEntity;
+
+/**
+ * BankAccountDao: 数据操作接口
+ *
+ * 这只是一个减少手工创建的模板文件
+ * 可以任意添加方法和实现, 更改作者和重定义类名
+ * <p/>@author Powered By Fluent Mybatis
+ */
+public interface BankAccountDao extends IBaseDao<BankAccountEntity> {
+}
