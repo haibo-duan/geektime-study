5c5f6155d7f491c8e9e05a32b201ef09e4ab37e6
haibo-duan <dhaibo1986@live.cn>
2021-10-20 10:29:52 +0800

    测试mybatis的缓存的project 自动生成了mybatis的orm

--- doc/JavaCourseStudy/week11/project/mybatis-cache-demo/src/main/java/com/dhb/mybatis/cache/demo/LevelOneCacheController.java
+++ doc/JavaCourseStudy/week11/project/mybatis-cache-demo/src/main/java/com/dhb/mybatis/cache/demo/LevelOneCacheController.java
@@ -5,11 +5,16 @@ import com.dhb.mybatis.cache.orm.dao.intf.BankAccountDao;
 import com.dhb.mybatis.cache.orm.entity.BankAccountEntity;
 import com.dhb.mybatis.cache.orm.mapper.BankAccountMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.util.Date;
 
 @RestController
@@ -24,6 +29,10 @@ public class LevelOneCacheController {
 
 	@Autowired
 	BankAccountMapper bankAccountMapper2;
+	
+	@Autowired
+	SqlSessionFactory factory;
+	
 	/**
 	 * mybatis中一级缓存的条件：
 	 *   1.配置mybatis.configuration.local-cache-scope: session / statement
@@ -66,26 +75,31 @@ public class LevelOneCacheController {
 
 	/**
 	 * 当两个sqlMapper对同一个表操作的时候：
-	 * 另外一个sqlMapper更新操作，会造成全局刷新
+	 * 另外一个session中更新了，对当前session中的缓存并不产生任何影响，这样可看出缓存对数据结果造成了污染。
+	 * 这也证明了，mybatis的一级缓存只在一个sqlSession中有效。 可以将一级缓存配置为statement,这样等价于变相关闭一级缓存。
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/testLevel1Cache2Mapper")
 	@Transactional
 	public String testLevel1Cache2Mapper(int id) {
-		BankAccountEntity accountEntity = bankAccountMapper.findById(id);
+		
+		SqlSession sqlSession = factory.openSession(true);
+		BankAccountMapper bankAccountMapper2 = sqlSession.getMapper(BankAccountMapper.class);
+		
+		BankAccountEntity accountEntity = bankAccountMapper2.findById(id);
 		log.info("1:"+JSONObject.toJSONString(accountEntity));
-		accountEntity = bankAccountMapper.findById(id);
-		log.info("1:"+JSONObject.toJSONString(accountEntity));
+		accountEntity = bankAccountMapper2.findById(id);
+		log.info("2:"+JSONObject.toJSONString(accountEntity));
 		BankAccountEntity updateEntity = new BankAccountEntity();
 		updateEntity.setId(1);
 		updateEntity.setUpdateTime(new Date());
-		log.info(" 再次查询相同的数据。。。");
-		bankAccountMapper2.updateById(updateEntity);
-		accountEntity = bankAccountMapper2.findById(id);
-		log.info("1:"+JSONObject.toJSONString(accountEntity));
+		bankAccountMapper.updateById(updateEntity);
+		log.info(" 通过bankAccountMapper 修改数据，再次查询相同的数据。。。");
 		accountEntity = bankAccountMapper.findById(id);
-		log.info("1:"+JSONObject.toJSONString(accountEntity));
+		log.info("3:"+JSONObject.toJSONString(accountEntity));
+		accountEntity = bankAccountMapper2.findById(id);
+		log.info("4:"+JSONObject.toJSONString(accountEntity));
 		return JSONObject.toJSONString(accountEntity);
 	}
 
