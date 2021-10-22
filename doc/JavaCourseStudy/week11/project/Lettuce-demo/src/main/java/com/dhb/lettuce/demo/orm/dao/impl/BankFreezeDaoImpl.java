ed1e832944ac22eb802e774a5190d48c1082ae64
haibo-duan <dhaibo1986@live.cn>
2021-10-22 15:48:52 +0800

    自动生成的orm代码

---
+++ doc/JavaCourseStudy/week11/project/Lettuce-demo/src/main/java/com/dhb/lettuce/demo/orm/dao/impl/BankFreezeDaoImpl.java
@@ -0,0 +1,16 @@
+package com.dhb.lettuce.demo.orm.dao.impl;
+
+import com.dhb.lettuce.demo.orm.dao.base.BankFreezeBaseDao;
+import com.dhb.lettuce.demo.orm.dao.intf.BankFreezeDao;
+import org.springframework.stereotype.Repository;
+
+/**
+ * BankFreezeDaoImpl: 数据操作接口实现
+ *
+ * 这只是一个减少手工创建的模板文件
+ * 可以任意添加方法和实现, 更改作者和重定义类名
+ * <p/>@author Powered By Fluent Mybatis
+ */
+@Repository
+public class BankFreezeDaoImpl extends BankFreezeBaseDao implements BankFreezeDao {
+}
