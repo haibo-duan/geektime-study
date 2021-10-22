49dc1cbfbcf54acc0ba21ccf20ad176ebac00d23
haibo-duan <dhaibo1986@live.cn>
2021-10-22 17:35:53 +0800

    配置文件和启动器

---
+++ doc/JavaCourseStudy/week11/project/redis-lock/src/main/java/com/dhb/redis/lock/RedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.dhb.redis.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLockApplication.class, args);
+	}
+
+}
