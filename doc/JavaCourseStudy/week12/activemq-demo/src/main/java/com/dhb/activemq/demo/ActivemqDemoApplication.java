c95f392257b4c4c21f2420b7e1eba2eba63fd20b
haibo-duan <dhaibo1986@live.cn>
2021-10-28 16:38:38 +0800

    启动器

---
+++ doc/JavaCourseStudy/week12/activemq-demo/src/main/java/com/dhb/activemq/demo/ActivemqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dhb.activemq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqDemoApplication.class, args);
+	}
+
+}
