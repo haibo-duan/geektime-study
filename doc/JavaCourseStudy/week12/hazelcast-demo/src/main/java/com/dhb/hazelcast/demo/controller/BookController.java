93ed0ca877b1ed3b0e4d830a47d5b08dc5daf779
haibo-duan <dhaibo1986@live.cn>
2021-10-30 22:45:32 +0800

    增加耗时时间

--- doc/JavaCourseStudy/week12/hazelcast-demo/src/main/java/com/dhb/hazelcast/demo/controller/BookController.java
+++ doc/JavaCourseStudy/week12/hazelcast-demo/src/main/java/com/dhb/hazelcast/demo/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.dhb.hazelcast.demo.controller;
 
 import com.dhb.hazelcast.demo.service.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
+@Slf4j
 public class BookController {
 
 	private final BookService bookService;
@@ -18,6 +21,11 @@ public class BookController {
 
 	@GetMapping("/{isbn}")
 	public String getBookNameByIsbn(@PathVariable("isbn") String isbn) {
-		return bookService.getBookNameByIsbn(isbn);
+		StopWatch watch = new StopWatch();
+		watch.start();
+		String result = bookService.getBookNameByIsbn(isbn);
+		watch.stop();
+		log.info("getBookNameByIsbn cost {} ",watch.prettyPrint());
+		return result;
 	}
 }
