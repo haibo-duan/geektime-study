e1cd2e2e17531352c70430bacbeb64177450e259
haibo-duan <dhaibo1986@live.cn>
2021-10-30 22:46:39 +0800

    三个Node

---
+++ doc/JavaCourseStudy/week12/hazelcast-demo/src/main/java/com/dhb/hazelcast/demo/HazelcastNode1Starter.java
@@ -0,0 +1,15 @@
+package com.dhb.hazelcast.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class HazelcastNode1Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastNode1Starter.class, args);
+	}
+
+}
---
+++ doc/JavaCourseStudy/week12/hazelcast-demo/src/main/java/com/dhb/hazelcast/demo/HazelcastNode2Starter.java
@@ -0,0 +1,15 @@
+package com.dhb.hazelcast.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class HazelcastNode2Starter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastNode2Starter.class, args);
+	}
+
+}
